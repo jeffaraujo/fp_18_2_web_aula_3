2730d7446150c89e2ff6c0325a023294dbc4ff54
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2018-10-08 20:47:04 -0300

    Incluíndo projeto API

---
+++ fp_stack.api/Controllers/PerguntasController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using fp_stack.core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace fp_stack.api.Controllers
+{
+    
+    public class PerguntasController : Controller
+    {
+        private readonly Context _context;
+
+        public PerguntasController(Context context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public List<Pergunta> Index()
+        {
+            return _context.Perguntas.ToList();
+        }
+
+
+    }
+}
--- fp_stack.api/Startup.cs
+++ fp_stack.api/Startup.cs
@@ -4,20 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using fp_stack.core.Models;
 using fp_stack.core.Services;
-using fp_stack.web.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace fp_stack.web
+namespace fp_stack.api
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,16 +24,12 @@ namespace fp_stack.web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             //Qualquer objeto que pedir uma instancia nova, ele criará uma instancia nova
             services.AddTransient<NoticiaService>();
 
-            //Para cada objeto 
-            //services.AddScoped
-            
-            //services.AddSingleton
-            
 
             var connection = @"Server=(localdb)\mssqllocaldb;Database=StackDB;Trusted_Connection=True;ConnectRetryCount=0";
             services.AddDbContext<Context>(options => options.UseSqlServer(connection));
@@ -51,14 +45,20 @@ namespace fp_stack.web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMeuLog();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
             app.UseStaticFiles();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Perguntas}/{action=Pergunta}/{id?}");
+            //});
+            //Mapeia automáticamente
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
