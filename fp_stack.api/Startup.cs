be453ce08e2d3f7bd4c8d647d78191d0ff302782
Jeferson da Silva Araujo <jeff.araujo@gmail.com>
2018-10-08 22:32:33 -0300

    Implementando na API o retorno em XML ao invés de JSON

--- fp_stack.api/Startup.cs
+++ fp_stack.api/Startup.cs
@@ -7,6 +7,7 @@ using fp_stack.core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -34,7 +35,11 @@ namespace fp_stack.api
             var connection = @"Server=(localdb)\mssqllocaldb;Database=StackDB;Trusted_Connection=True;ConnectRetryCount=0";
             services.AddDbContext<Context>(options => options.UseSqlServer(connection));
 
-            services.AddMvc();
+            services.AddMvc(
+                options => {
+                    options.RespectBrowserAcceptHeader = true; //Respeite o cliente
+                    options.OutputFormatters.Add(new XmlSerializerOutputFormatter());//Convertendo de JSON para XML
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
